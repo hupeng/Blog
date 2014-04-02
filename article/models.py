3280ec28958a01322a6487da1458f808bfc2b9d8
hupeng <danny.hupeng@gmail.com>
2014-04-02 17:33:12 +0800

    add article model

--- article/models.py
+++ article/models.py
@@ -1,3 +1,36 @@
+#-*- coding: utf-8 -*-
+#!/usr/local/bin/python
+
 from django.db import models
 
-# Create your models here.
+class Article_Manager(models.Manager):
+    pass
+
+class DB_Article(models.Model):
+    title   = models.CharField(max_length=100)
+    author  = models.CharField(max_length=30, default="Danny")
+    publish = models.DateTimeField(auto_now_add=True)
+    comment = models.IntegerField(default=0)
+    status  = models.IntegerField(default=0)    # 0 --- 草稿  1 --- 正式发布
+    
+    objects = Article_Manager()
+
+
+class Labels_Manager(models.Manager):
+    pass
+
+class DB_Labels(models.Model):
+    name    = models.CharField(max_length=10)
+    amount  = models.IntegerField(default=1)
+    
+    objects = Labels_Manager()
+
+
+class Article_Labels_Manager(models.Manager):
+    pass
+
+class DB_Article_Labels(models.Model):
+    article_id = models.CharField(max_length=20)
+    label_id   = models.CharField(max_length=20)
+    
+    objects    = Article_Labels_Manager()
